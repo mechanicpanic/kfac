fee31715270aff4282099859a498649cae2dadab
Annae <mechanicpanic@users.noreply.github.com>
2024-04-30 17:47:33 +0200

    upd

--- kfac/python/ops/layer_collection.py
+++ kfac/python/ops/layer_collection.py
@@ -527,6 +527,8 @@ class LayerCollection(object):
         for var in utils.ensure_sequence(params)
     }
     for variable in utils.ensure_sequence(layer_key):
+      if isinstance(variable, tf.Variable):
+        variable = variable.ref()
       if variable in variable_to_block:
         prev_key, prev_block = variable_to_block[variable]
         raise ValueError(
